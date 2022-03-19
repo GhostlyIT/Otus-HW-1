f99bbc7d4516f4ff7515e48c4a30de7b7fe8d3e2
ghostlyit <supermaximal23@gmail.com>
2022-03-19 22:15:23 +0300

    Init

---
+++ src/main/java/sunsetcode/App.java
@@ -0,0 +1,13 @@
+package sunsetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
